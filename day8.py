6387141467a9ddbbedaa74e94c9869f016ab9280
Jacques Uber <uberj@miradorfin.com>
2018-12-10 10:49:35 -0800

    day 8 part 2

--- day8.py
+++ day8.py
@@ -5,12 +5,7 @@ def challenge1():
 
 def challenge2():
     with open("inputs/license.txt", "r") as fd:
-        pass
-
-
-def build_tree(param):
-    param.split(" ")
-    pass
+        return root_node_value(fd.read())
 
 
 def sum_meta(param):
@@ -31,6 +26,32 @@ def _sum_meta(param):
     return meta_sum
 
 
+def root_node_value(param):
+    param = list(map(int, param.split(" ")))
+    return _root_node_value(param)
+
+
+def _root_node_value(param):
+    num_children = param.pop(0)
+    num_meta = param.pop(0)
+    child_values = []
+    for i in range(num_children):
+        child_values.append(_root_node_value(param))
+
+    if num_children == 0:
+        meta_sum = 0
+        for i in range(num_meta):
+            meta_sum += param.pop(0)
+        return meta_sum
+    else:
+        node_value = 0
+        for i in range(num_meta):
+            meta = param.pop(0)
+            if meta <= len(child_values):
+                node_value += child_values[meta - 1]
+        return node_value
+
+
 if __name__ == "__main__":
     test_meta_sum = sum_meta("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")
     assert test_meta_sum == 138, test_meta_sum
@@ -38,3 +59,8 @@ if __name__ == "__main__":
     c1 = challenge1()
     assert c1 == 49180
 
+    test_node_value = root_node_value("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2")
+    assert test_node_value == 66, test_node_value
+
+    c2 = challenge2()
+    assert c2 == 20611
